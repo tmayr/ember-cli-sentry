8b730efaf94ef96dcdd04f3f3dc2d947777c5a96
Tomas Mayr <tmayr@tmayr.com>
2015-06-25 19:06:33 -0300

    not loading raven if in development mode, using raven ember bundle instead of own implementation, usingsemver to compare versions

--- app/initializers/raven-setup.js
+++ app/initializers/raven-setup.js
@@ -1,36 +1,35 @@
 /* global Raven */
-
 import config from '../config/environment';
 import Ember from 'ember';
 
 export function initialize() {
-  // Disable for development
-  if (config.sentry.development) {
+  if(config.sentry.development){
     return;
   }
-
-  var _onerror;
 
   Raven.config(config.sentry.dsn, {
     release: config.APP.version,
     whitelistUrls: config.sentry.whitelistUrls
   }).install();
 
-  _onerror = Ember.onerror;
-  Ember.onerror = function(error){
-    Raven.captureException(error);
-    if (typeof _onerror === 'function') {
-      _onerror.call(this, error);
-    }
-  }
+  if(!config.sentryBundle){
+    var _onerror = Ember.onerror;
 
-  Ember.RSVP.on('error', function (reason) {
-    if (reason instanceof Error) {
-      Raven.captureException(reason, {extra: {context: 'Unhandled Promise error detected'}});
-    } else {
-      Raven.captureMessage('Unhandled Promise error detected', {extra: {reason: reason}});
+    Ember.onerror = function(error){
+      Raven.captureException(error);
+      if (typeof _onerror === 'function') {
+        _onerror.call(this, error);
+      }
     }
-  });
+
+    Ember.RSVP.on('error', function (reason) {
+      if (reason instanceof Error) {
+        Raven.captureException(reason, {extra: {context: 'Unhandled Promise error detected'}});
+      } else {
+        Raven.captureMessage('Unhandled Promise error detected', {extra: {reason: reason}});
+      }
+    });
+  }
 }
 
 export default {
--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 /* jshint node: true */
 'use strict';
+var semver = require('semver');
 
 module.exports = {
 
   name: 'ember-cli-sentry',
 
   contentFor: function(type, config){
-    if (type === 'body' && !config.sentry.skipCdn) {
-      return '<script src="' + config.sentry.cdn + '/' + config.sentry.version + '/jquery,native/raven.min.js"></script>';
+    var bundle;
+
+    /*
+    * 1.1.19 is the version where sentry started including
+    * the correct ember debug version, if we're using a
+    * greater version than 1.1.19, we just use their version
+    */
+    if(semver.gte(config.sentry.version, '1.1.19')){
+      config.sentryBundle = true;
+      bundle = 'ember,jquery,native';
+    } else {
+      config.sentryBundle = false;
+      bundle = 'jquery,native';
+    }
+
+    if (type === 'body' && !config.sentry.skipCdn && !config.sentry.development) {
+      return '<script src="' + config.sentry.cdn + '/' + config.sentry.version + '/' + bundle + '/raven.min.js"></script>';
     }
   }
 
